43db82f970195a740d8262a50afcebae1a5739d1
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-07-27 15:01:43 +0300

    * Repairing Errors in Hadith content but it views the first hadith only

--- .dart_tool/flutter_build/58831e78c99864515d95f10b1445ee53/install_code_assets.d
+++
@@ -1,1 +0,0 @@
- D:\\Flutter_Deploma\\assignmenthb\\.dart_tool\\flutter_build\\58831e78c99864515d95f10b1445ee53\\native_assets.json: 
--- lib/models/hadithDataModel.dart
+++
@@ -1,6 +0,0 @@
-class HadithDataModel {
-  String title;
-  String content;
-
-  HadithDataModel({required this.title, required this.content});
-}
